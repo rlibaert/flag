cb311313980b47de95cfb0edb242dd112d25c8aa
Romain Libaert <git@rlibaert.dev>
2026-05-28 16:00:19 +0000

    refacto: use fmt.Sscan to parse most basic types

--- values/basic.go
+++ values/basic.go
@@ -3,7 +3,6 @@ package values
 import (
 	"flag"
 	"fmt"
-	"strconv"
 )
 
 type basic = interface {
@@ -15,76 +14,11 @@ type basic = interface {
 		string | []byte
 }
 
-func parseBasic[T basic](s string) (T, error) { //nolint: funlen // long but still readable
+func parseBasic[T basic](s string) (T, error) {
 	var v T
 	var err error
 
 	switch p := any(&v).(type) {
-	case *bool:
-		*p, err = strconv.ParseBool(s)
-
-	case *complex64:
-		var tmp complex128
-		tmp, err = strconv.ParseComplex(s, 64)
-		*p = complex64(tmp)
-
-	case *complex128:
-		*p, err = strconv.ParseComplex(s, 128)
-
-	case *int:
-		var i int64
-		i, err = strconv.ParseInt(s, 0, 0)
-		*p = int(i)
-
-	case *int8:
-		var i int64
-		i, err = strconv.ParseInt(s, 0, 8)
-		*p = int8(i)
-
-	case *int16:
-		var i int64
-		i, err = strconv.ParseInt(s, 0, 16)
-		*p = int16(i)
-
-	case *int32:
-		var i int64
-		i, err = strconv.ParseInt(s, 0, 32)
-		*p = int32(i)
-
-	case *int64:
-		*p, err = strconv.ParseInt(s, 0, 64)
-
-	case *uint:
-		var u uint64
-		u, err = strconv.ParseUint(s, 0, 0)
-		*p = uint(u)
-
-	case *uint8:
-		var u uint64
-		u, err = strconv.ParseUint(s, 0, 8)
-		*p = uint8(u)
-
-	case *uint16:
-		var u uint64
-		u, err = strconv.ParseUint(s, 0, 16)
-		*p = uint16(u)
-
-	case *uint32:
-		var u uint64
-		u, err = strconv.ParseUint(s, 0, 32)
-		*p = uint32(u)
-
-	case *uint64:
-		*p, err = strconv.ParseUint(s, 0, 64)
-
-	case *float32:
-		var f float64
-		f, err = strconv.ParseFloat(s, 32)
-		*p = float32(f)
-
-	case *float64:
-		*p, err = strconv.ParseFloat(s, 64)
-
 	case *string:
 		*p = s
 
@@ -92,7 +26,7 @@ func parseBasic[T basic](s string) (T, error) { //nolint: funlen // long but sti
 		*p = []byte(s)
 
 	default:
-		panic("values: unsupported type")
+		_, err = fmt.Sscan(s, p)
 	}
 
 	return v, err
